72fbd905c3eaf250f9b3debaf8e6871cac16c6fe
tomokb <tomobuchberg@gmail.com>
2021-07-29 02:15:09 -0700

    Add randomize and monochrome color functionality

--- main.js
+++ main.js
@@ -1,75 +1,86 @@
+// -- Default values
 const canvasMinSize = 16;
 const canvasMaxSize = 100;
-const canvasPixelWidth = 480;
 let canvasSize = canvasMinSize;
+const canvasPixelWidth = 480;
+const blankTileColor = '#fff';
+let colorPickerValue = '#000';
+let colorMode = 'user-select';
+
+// -- DOM nodes
 const canvas = document.querySelector("#canvas");
 const newCanvasButton = document.querySelector("#new-canvas");
 const chooseColorButton = document.querySelector("#choose-color");
 
-const newCanvasOptions = document.querySelector("#new-canvas-options");
-const colorOptions = document.querySelector("#color-options");
+const newCanvasModal = document.querySelector("#new-canvas-modal");
+const colorModal = document.querySelector("#color-modal");
 
+// TODO: refactor (decouple JS logic from HTML)
 const span1 = document.querySelectorAll(".close")[0];
 const span2 = document.querySelectorAll(".close")[1];
 
 const canvasRange = document.querySelector('input[name=canvas-range]');
 const canvasRangeLabel = document.querySelector('#canvas-size');
 
-const colorOption = document.querySelector('input[name=color]');
-const colorPreview = document.querySelector('#color-preview');
-let currentColor = '#000';
-let previewColor = '#000';
-//colorPreview.style['background-color'] = `${color.value}`;
+const colorOptions = document.querySelectorAll('input[name=color]'); // Get all color options
+const colorPicker = document.querySelector('#color-picker');
 
-// const monochrome = hsl(0, 100, 10);
+// -- Start
 window.onload = init;
 
+// -- Functions
 function init() {
     createGrid();
-    newCanvasButton.addEventListener('click', displayNewCanvasOptions);
-    chooseColorButton.addEventListener('click', displayColorOptions);
+    newCanvasButton.addEventListener('click', displayNewCanvasModal);
+    chooseColorButton.addEventListener('click', displayColorModal);
 
     // TODO: refactor (decouple JS logic from HTML)
-    span1.addEventListener('click', () => closeModal(newCanvasOptions));
-    span2.addEventListener('click', () => closeModal(colorOptions));
+    span1.addEventListener('click', () => closeModal(newCanvasModal));
+    span2.addEventListener('click', () => closeModal(colorModal));
 
     window.addEventListener('click', (e) => {
-        if (e.target === newCanvasOptions) {
-            closeModal(newCanvasOptions);
+        if (e.target === newCanvasModal) {
+            closeModal(newCanvasModal);
         }
-        if (e.target === colorOptions) {
-            closeModal(colorOptions);
+        if (e.target === colorModal) {
+            closeModal(colorModal);
         }
     });
 
-    colorPreview.addEventListener('input', updatePreviewColor)
-    colorOption.addEventListener('input', updateCurrentColor);
+    colorPicker.addEventListener('input', updateColorPickerValue);
+    colorOptions.forEach(option => option.addEventListener('input', updateColorMode));
     canvasRange.addEventListener('input', updateCanvasRangeLabel);
 }
 
-function updatePreviewColor(e) {
-
+// Called whenever user changes color-picker value
+function updateColorPickerValue(e) {
+    colorPickerValue = e.target.value;
 }
 
-function updateCurrentColor(e) {
-    switch (e.target.value) {
-        case "user-select":
-            currentColor = colorPreview.value;
-            break;
-        case "randomize": 
-            break;
-        case "monochrome":
-            break;
-    }
+// Called whenever user selects a color option
+function updateColorMode(e) {
+    colorMode = e.target.value;
 }
 
 function randomizeColor() {
-    return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    let zeroTo255 = () => Math.floor(Math.random() * 256);
+    return `rgb(${zeroTo255()}, ${zeroTo255()}, ${zeroTo255()})`;
 }
 
 // Each pass adds 10% black (reduces lightness by 10%)
-function monochromeColor() {
-    let lightnessPercentage = 10;
+function monochromeColor(tile) {
+    const maxLightness = 255;
+
+    let lightnessPercentage = parseFloat(tile.getAttribute('data-lightness'));
+    if (isNaN(lightnessPercentage)) {
+        lightnessPercentage = 0.9;
+        tile.setAttribute('data-lightness', `${lightnessPercentage}`);
+    } else if (lightnessPercentage > 0.0) {
+        tile.setAttribute('data-lightness', `${lightnessPercentage - 0.1}`);
+    }
+    const tileColor = (maxLightness * lightnessPercentage).toFixed(2);
+    console.log(tileColor);
+    return `rgb(${tileColor}, ${tileColor}, ${tileColor})`;
 }
 
 function updateCanvasRangeLabel(e) {
@@ -88,6 +99,7 @@ function updateCanvasSize(newSize) {
 
 // Default size is 16 x 16 tiles
 function createGrid() {
+    // console.log("Creating grid...");
     for (let i = 0; i < canvasSize * canvasSize; i++) {
         const tile = document.createElement('div');
         canvas.appendChild(tile);
@@ -96,22 +108,33 @@ function createGrid() {
     // CSS
     const tileSize = canvasPixelWidth / canvasSize;
     canvas.style['grid-template'] = `repeat(${canvasSize}, ${tileSize}px) / repeat(${canvasSize}, ${tileSize}px)`;
-    console.log(`repeat(${canvasSize}, ${tileSize}px) / repeat(${canvasSize}, ${tileSize}px)`);
 }
 
 function deleteGrid() {
-    console.log("Deleting grid...");
+    // console.log("Deleting grid...");
     while(canvas.firstChild) {
         canvas.removeChild(canvas.firstChild);
     }
 }
 
 function fillTile(tile) {
-    tile.classList.add('filled');
+    let currentColorValue = '#000'; // Set color value with CSS colors
+    switch (colorMode) {
+        case 'user-select': 
+            currentColorValue = colorPickerValue;
+            break;
+        case 'randomize': 
+            currentColorValue = randomizeColor();
+            break;
+        case 'monochrome':
+            currentColorValue = monochromeColor(tile);
+            break;
+    }
+    tile.style['background-color'] = `${currentColorValue}`;
 }
 
 function eraseTile(tile) {
-    tile.classList.remove('filled');
+    tile.style['background-color'] = `${blankTileColor}`;
 }
 
 function clearCanvas() {
@@ -123,24 +146,22 @@ function clearCanvas() {
     for (let i = 0; i < tiles.length; i++) {
         let tile = tiles[i];
         if (tile.classList !== undefined) { // tile.classList returns undefined when tile is text and not an element
-            if (tile.classList.contains("filled")) {
-                eraseTile(tile);
-            }
+            eraseTile(tile);
         } 
     }
 }
 
-function displayNewCanvasOptions() {
-    displayOptions(newCanvasOptions);
+function displayNewCanvasModal() {
+    displayModal(newCanvasModal);
     clearCanvas();
 }
 
-function displayColorOptions() {
-    displayOptions(colorOptions);
+function displayColorModal() {
+    displayModal(colorModal);
 }
 
-function displayOptions(optionsPanel) {
-    optionsPanel.style.display = "block";
+function displayModal(modal) {
+    modal.style.display = "block";
     return true;
 }
 
@@ -148,17 +169,17 @@ function closeModal(modal) {
     modal.style.display = "none";
 }
 
-function createModal(id) {
-    const modal = document.createElement("div");
-    const modalContent = document.createElement("div");
-    const cancel = document.createElement("span");
-    span.textContent = '&times;';
-    modalContent.appendChild(cancel);
-    modal.appendChild(modalContent);
+// function createModal(id) {
+//     const modal = document.createElement("div");
+//     const modalContent = document.createElement("div");
+//     const cancel = document.createElement("span");
+//     span.textContent = '&times;';
+//     modalContent.appendChild(cancel);
+//     modal.appendChild(modalContent);
 
-    modal.setAttribute('id', id);
-    modal.setAttribute('class', 'modal');
-    modalContent.setAttribute('class', 'modal-content');
-    cancel.setAttribute('class', 'close');
-    return modal;
-}
+//     modal.setAttribute('id', id);
+//     modal.setAttribute('class', 'modal');
+//     modalContent.setAttribute('class', 'modal-content');
+//     cancel.setAttribute('class', 'close');
+//     return modal;
+// }
